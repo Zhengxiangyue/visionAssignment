1468837bb18641981f51648937aba570271d8d78
zhengxiangyue <zhengxiangyue@aspirecn.com>
2018-02-01 00:04:00 -0500

    no message

--- multiscale.m
+++ multiscale.m
@@ -5,8 +5,8 @@ clear;
 % input image folder
 imfolder = './data/';
 
-% name of the input file
-imname = '00153u.tif';
+% name of the input file, jpg
+imname = '01725u.jpg';
 
 % read in the image
 fullim = imread(strcat(imfolder,imname));
@@ -15,13 +15,13 @@ fullim = imread(strcat(imfolder,imname));
 [R, G, B] = seperate_image(fullim);
 
 %  calculate how many pixel G and B should shift
- [gi, gj, bi, bj] = multi_scale_shift_result(R, G, B, 0, 0, 0, 0, 15);
+[gi, gj, bi, bj] = multi_scale_shift_result(R, G, B, 0, 0, 0, 0, 15);
  
- aligned_image = cat(3, R, circshift(G,[gi gj]), circshift(B,[bi bj]));
+aligned_image = cat(3, R, circshift(G,[gi gj]), circshift(B,[bi bj]));
  
- color_image_folder = './result/';
+color_image_folder = './result/';
  
- imshow(uint8(aligned_image));
+imshow(uint8(aligned_image));
 
- imwrite(uint8(aligned_image),strcat(color_image_folder,'colored_single_scale_new_',imname));
+imwrite(uint8(aligned_image),strcat(color_image_folder,'colored_multi_scale_new_',imname));
 
