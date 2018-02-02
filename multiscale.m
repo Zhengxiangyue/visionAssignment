273840a7873024c86359940e064b232d9073fd0d
zhengxiangyue <zhengxiangyue@aspirecn.com>
2018-02-01 22:32:27 -0500

    no message

--- multiscale.m
+++ multiscale.m
@@ -14,14 +14,16 @@ fullim = imread(strcat(imfolder,imname));
 % seperate the image into three channels
 [R, G, B] = seperate_image(fullim);
 
+imshow(uint8(scaleBy2(R)));
+
 %  calculate how many pixel G and B should shift
 [gi, gj, bi, bj] = multi_scale_shift_result(R, G, B, 0, 0, 0, 0, 15);
- 
+  
 aligned_image = cat(3, R, circshift(G,[gi gj]), circshift(B,[bi bj]));
- 
+  
 color_image_folder = './result/';
- 
+  
 imshow(uint8(aligned_image));
-
+ 
 imwrite(uint8(aligned_image),strcat(color_image_folder,'colored_multi_scale_new_',imname));
 
--- scaleBy2.m
+++ scaleBy2.m
@@ -1,3 +1,6 @@
 function output = scaleBy2(input)
-    output = impyramid(input, 'reduce');
+%   output = impyramid(input, 'reduce');
+    [m,n] = size(input);
+    input = imgaussfilt(input, 2);
+    output = input(1:2:m, 1:2:n);
 end
