3f57193a1189711a32407d9a43a9e958820683e9
zhengxiangyue <zhengxiangyue@aspirecn.com>
2018-02-06 08:30:37 -0500

    CROP the main page's and auto contrast and auto white balance

--- auto_white_balance.m
+++ auto_white_balance.m
@@ -4,7 +4,8 @@ R = RGB(:,:,1);      G = RGB(:,:,2);      B = RGB(:,:,3);
 R_ave = mean(mean(R));   
 G_ave = mean(mean(G));   
 B_ave = mean(mean(B));  
-K_ave = (Rave + Gave + Bave) / 3;  
+
+K_ave = (R_ave + G_ave + B_ave) / 3;  
 R1 = (K_ave/R_ave)*R; 
 G1 = (K_ave/G_ave)*G; 
 B1 = (K_ave/B_ave)*B;   
